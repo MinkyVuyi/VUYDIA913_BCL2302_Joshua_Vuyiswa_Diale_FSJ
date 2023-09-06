9741ea72f610e3a68a881f2f4c5d5831f63af36a
MinkyVuyi <dialevuyi@gmail.com>
2023-09-06 06:41:07 +0200

    fixed folders and added new content

--- Udemy-Nextjs/next-events-app/pages/index.js
+++ Udemy-Nextjs/next-events-app/pages/index.js
@@ -1,7 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from '../events/event-list';
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-    <h1>The Home Page</h1>
+    <EventList items={featuredEvents} />
     </div>
   );
 }
