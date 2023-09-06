24c00fd6fb70f6f892d331a565812dbf4d114da9
MinkyVuyi <dialevuyi@gmail.com>
2023-09-06 10:09:09 +0200

    added a button

--- Udemy-Nextjs/next-events-app/components/ui/button.js
+++ Udemy-Nextjs/next-events-app/components/ui/button.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 
+import classes from './button.module.css';
+
 function Button(props) {
     return (
-        <Link href={props.link}>{props.children}</Link>
+        <Link href={props.link}>
+            <a className={classes.btn}>{props.children}</a>
+        </Link>
     );
 }
 
