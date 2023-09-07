62d5fca76c9936c39b9686daa8a2bdff1298d32f
MinkyVuyi <dialevuyi@gmail.com>
2023-09-07 06:48:46 +0200

    header

--- Udemy-Nextjs/next-events-app/pages/events/[eventId].js
+++ Udemy-Nextjs/next-events-app/pages/events/[eventId].js
@@ -1,5 +1,10 @@
-import { getEventById } from '@/dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import { getEventById } from '../../dummy-data';
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 
 function EventDetailPage() {
     const router = useRouter();
@@ -14,9 +19,18 @@ function EventDetailPage() {
     }
 
     return (
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title} />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+
+
+        </Fragment>
     );
 }
 
