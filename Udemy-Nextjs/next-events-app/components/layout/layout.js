f964bec32bc21f3049606ab78fc12822827f7dc3
MinkyVuyi <dialevuyi@gmail.com>
2023-09-07 08:35:44 +0200

    layout

--- Udemy-Nextjs/next-events-app/components/layout/layout.js
+++ Udemy-Nextjs/next-events-app/components/layout/layout.js
@@ -1,8 +1,8 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import MainHeader from "./main-header";
+import MainHeader from './main-header';
 
-function Layout() {
+function Layout(props) {
     return (
         <Fragment>
             <MainHeader />
--- Udemy-Nextjs/next-events-app/components/layout/main-header.js
+++ Udemy-Nextjs/next-events-app/components/layout/main-header.js
@@ -1,12 +1,13 @@
 import Link from 'next/link;'
 
-import classes from '/layout/main-header.module.css';
+import classes from './main-header.module.css';
+// import Layout from './layout';
 
 function MainHeader() {
 return (
     <header className={classes.header}>
         <div className={classes.logo}>
-            <Link href='/'>NextEvent</Link>
+            <Link href='/'>NextEvents</Link>
         </div>
         <nav className={classes.navigation}> 
             <ul>
