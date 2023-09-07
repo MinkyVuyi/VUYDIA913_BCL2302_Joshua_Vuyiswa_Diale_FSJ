e53f008cc0d0acb56873a9d5f5838e44d312dec4
MinkyVuyi <dialevuyi@gmail.com>
2023-09-07 05:54:34 +0200

    event details

--- Udemy-Nextjs/next-events-app/pages/events/[eventId].js
+++ Udemy-Nextjs/next-events-app/pages/events/[eventId].js
@@ -1,4 +1,18 @@
+import { getEventById } from '@/dummy-data';
+import { useRouter } from 'next/router';
+
 function EventDetailPage() {
+    const router = useRouter();
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return (
+            <p>No event found!</p>
+        )
+    }
+
     return (
         <div>
             <h1>Event Detail</h1>
