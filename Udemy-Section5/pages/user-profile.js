366c6f8e1d58f23ae3be7815703d03b50dcebd67
MinkyVuyi <dialevuyi@gmail.com>
2023-09-17 08:10:13 +0200

    max

--- Udemy-Section5/pages/user-profile.js
+++ Udemy-Section5/pages/user-profile.js
@@ -1,5 +1,13 @@
 function UserProfilePage(props) {
-
+return <h1>{props.username}</h1>
 }
 
 export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+    return {
+        props: {
+            username: 'Max'
+        }
+    };
+}
