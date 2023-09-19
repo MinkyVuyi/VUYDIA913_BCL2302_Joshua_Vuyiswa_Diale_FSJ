6e292e21dd9daf02edf31c152b2b86152139f03c
MinkyVuyi <dialevuyi@gmail.com>
2023-09-19 15:23:01 +0200

    getStaticProps added

--- Udemy-Section5/pages/last-sale.js
+++ Udemy-Section5/pages/last-sale.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useSWR } from 'swr';
+import { getStaticProps } from './../../nextjs-course-code/pages/index';
 
-function LastSalesPage() {
+function LastSalesPage(props) {
     const [sales, setSales] = useState();
     // const [loading, setLoading] = useState(false);
 
@@ -10,30 +11,32 @@ function LastSalesPage() {
     useEffect(() => {
         if (data) {
             const transformedSales = [];
+
             for (const key in data) {
-            transformedSales.push({
-                id: key, 
-                username: data[key].username, 
-                volume: data[key].volume,
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume,
                 });
             }
+
             setSales(transformedSales);
         }
     }, [data]);
 
     // useEffect(() => {
     //     setIsLoading(true);
-    //     fetch('https://nextjs-course-fb252-default-rtdb.firebaseio.com/sales.json').then(response => response.json()).then(data => {
-    //         const transformedSales = [];
+    // fetch('https://nextjs-course-fb252-default-rtdb.firebaseio.com/sales.json').then(response => response.json()).then(data => {
+    //     const transformedSales = [];
 
-    //         for (const key in data) {
-    //            transformedSales.push({id: key, username: data[key].username, volume: data[key].volume,
-    //         });
-    //         }
-
-    //         setSales(transformedSales);
-    //         setIsLoading(false);
+    //     for (const key in data) {
+    //        transformedSales.push({id: key, username: data[key].username, volume: data[key].volume,
     //     });
+    //     }
+
+    //     setSales(transformedSales);
+    //     setIsLoading(false);
+    // });
     // }, []);
 
     if (error) {
@@ -46,12 +49,31 @@ function LastSalesPage() {
 
     return (
         <ul>
-          {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}  
-          
+            {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
+
         </ul>
     )
 }
 
+export async function getStaticProps() {
+    const response = await fetch('https://nextjs-course-fb252-default-rtdb.firebaseio.com/sales.json');
+    const data = await response.json();
+
+    const transformedSales = [];
+
+    for (const key in data) {
+        transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+        });
+    }
+
+    return { props: { sales: transformedSales }, revalidate: 10 };
+
+};
+
 export default LastSalesPage;
 
 //$ for cosmetics
+//"getStaticProps" is not a React component therefore we can't use hooks in that function.
