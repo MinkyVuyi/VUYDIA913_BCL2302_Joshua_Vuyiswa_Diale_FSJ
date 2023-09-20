f62fb6893f06b83a1ceb897647c76c68eef9146e
MinkyVuyi <dialevuyi@gmail.com>
2023-09-20 20:01:19 +0200

    events

--- Udemy-Section7/pages/_documents.js
+++ Udemy-Section7/pages/_documents.js
@@ -2,10 +2,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
-        return(
+        return (
             <Html lang='en'>
                 <Head />
                 <body>
+                    <div id="overlays" />
                     <Main />
                     <NextScript />
                 </body>
