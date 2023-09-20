1006d90c2113c1ca0d181e02ae6db9551c2a2909
MinkyVuyi <dialevuyi@gmail.com>
2023-09-20 10:00:46 +0200

    events

--- Udemy-Section6/pages/events/[eventId].js
+++ Udemy-Section6/pages/events/[eventId].js
@@ -1,17 +1,14 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
+import { getStaticProps } from './../../../nextjs-course-code/pages/index';
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -37,4 +34,30 @@ function EventDetailPage() {
   );
 }
 
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map(event => ({ params: {eventId: event.id} }));
+
+  return {
+    paths: paths,
+    fallback: false
+  };
+}
+
 export default EventDetailPage;
+
+
+//context is needed on the getStaticProps function because we'll need to know for which specific event Id we want to load event data.
