e05a2726df80e60e33bb1f87cfa7a17e9aef315f
MinkyVuyi <dialevuyi@gmail.com>
2023-09-26 15:21:41 +0200

    module.css

--- Udemy-Section11/components/layout/layout.js
+++ Udemy-Section11/components/layout/layout.js
@@ -1,8 +1,12 @@
-function Layout() {
+import { Fragment } from 'react';
+
+import MainNavigation from './main-navigation';
+
+function Layout(props) {
 return (
     <Fragment>
         <MainNavigation />
-        
+        <main>{props.children}</main>
     </Fragment>
 )
 }
