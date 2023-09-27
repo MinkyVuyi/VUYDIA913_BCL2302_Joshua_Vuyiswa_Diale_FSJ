e8a1cef07f325cd8c5e9a9d4011b95c9addfea33
MinkyVuyi <dialevuyi@gmail.com>
2023-09-27 10:07:38 +0200

    posts

--- Udemy-Section11/pages/index.js
+++ Udemy-Section11/pages/index.js
@@ -1,12 +1,36 @@
 import { Fragment } from 'react';
-// import FeaturedPosts from '../components/home-page/featured-posts';
+
+import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
+
+const DUMMY_POSTS =[
+    { slug: 'getting-started-with-nextjs', 
+    title: 'Getting Started with NextJS', image:'getting-started-with-nextjs', 
+    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.', 
+    date:'2023-09-26', 
+},
+{ slug: 'getting-started-with-nextjs2', 
+    title: 'Getting Started with NextJS', image:'getting-started-with-nextjs', 
+    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.', 
+    date:'2023-09-26', 
+},
+{ slug: 'getting-started-with-nextjs3', 
+    title: 'Getting Started with NextJS', image:'getting-started-with-nextjs', 
+    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.', 
+    date:'2023-09-26', 
+},
+{ slug: 'getting-started-with-nextjs4', 
+    title: 'Getting Started with NextJS', image:'getting-started-with-nextjs', 
+    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.', 
+    date:'2023-09-26', 
+},
+];
 
 function HomePage() {
     return (
         <Fragment>
             <Hero />
-            
+            <FeaturedPosts posts={DUMMY_POSTS} />
         </Fragment>
     )
 }
@@ -15,4 +39,3 @@ export default HomePage;
 
 // 1) Hero Section - where we present ourselves
 // 2) Featured Posts
-// <FeaturedPosts />
