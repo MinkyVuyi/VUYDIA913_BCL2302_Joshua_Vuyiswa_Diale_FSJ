2443be6454ec07204ece98c75da18f513e754754
MinkyVuyi <dialevuyi@gmail.com>
2023-09-28 09:59:27 +0200

    database

--- Udemy-Section11/pages/api/contact.js
+++ Udemy-Section11/pages/api/contact.js
@@ -1,4 +1,7 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -12,6 +15,40 @@ function handler(req, res) {
             res.status(422).json({ message: 'Invalid input.' });
             return;
         }
+
+        // Store it in a database
+        const newMessage = {
+            email,
+            name,
+            message,
+        };
+
+        let client;
+
+        try {
+            client = await MongoClient.connect('mongodb+srv://vuyiswadiale:JyFUJSOZv7LJLseT@cluster0.tpfksdf.mongodb.net/my-site?retryWrites=true&w=majority');
+        } catch (error) {
+            res.status(500).json({ message: 'Could not connect to the database.' });
+            return;
+        }
+
+        const db = client.db();
+
+        try {
+
+            const result = db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({ message: 'Storing message failed!' });
+            return;
+        }
+
+        client.close();
+
+        res
+        .status(201)
+        .json({ message: 'Successfully stored message!', message: newMessage });
     }
 }
 
